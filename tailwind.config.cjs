cc18ccc0ad935cf7f6bab4f72ceef1f55d9b1afd
Sam Scolari <56174573+Sam-Scolari@users.noreply.github.com>
2023-04-09 13:43:25 -0500

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      white: "white",
+      beige: "#FAFAE3",
+      black: "#322C23",
+      red: "#E20010",
+      maroon: "#C3000E",
+    },
+    fontFamily: {
+      "crimson-pro": ["CrimsonPro"],
+    },
+  },
+  plugins: [],
+};
